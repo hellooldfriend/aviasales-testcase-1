382f85ab69d7b323e280ac140c61638ec1da6e50
Samat <samat1989@mail.ru>
2021-03-23 19:05:17 +0300

    Refactored requests to API logic

---
+++ src/api/api.tsx
@@ -0,0 +1,31 @@
+const SEARCH_ID_URL = 'https://front-test.beta.aviasales.ru/search'
+const TICKETS_URL = 'https://front-test.beta.aviasales.ru/tickets'
+
+
+const api = {
+    getSearchID: async () => {
+        try {
+            const response = await fetch(SEARCH_ID_URL)
+            return response.json()
+        } catch {
+            throw new Error('Failed to fetch searchID')
+        }
+    },
+
+    getTickets: async () => {
+        try {
+            const { searchId } = await api.getSearchID()
+            const url = `${TICKETS_URL}?searchId=${searchId}`
+            const response = await fetch(url)
+            return response.json()
+        } catch {
+            throw new Error('Failed to fetch tickets')
+            return {
+                tickets: [],
+                stop: true,
+            }
+        }
+    }
+}
+
+export default api
